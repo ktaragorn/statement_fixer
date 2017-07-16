b0e65e0a897f37806cbae1609f4e4444741219c8
Karthik T <karthikt.holmes+github@gmail.com>
2017-07-16 18:08:23 +0800

    Arguement parsing for filename, bank, type

--- src/statement_fixer.cr
+++ src/statement_fixer.cr
@@ -1,7 +1,17 @@
 require "./statement_fixer/*"
 require "./statement_fixer/statement/*"
 require "./statement_fixer/parsers/*"
+require "optarg"
+
+class Options < Optarg::Model
+  arg "csv_file"
+  string %w(-b --bank)
+  string %w(-t --type)
+end
 
 module StatementFixer
-  puts Parser.types
+  args = Options.parse(ARGV)
+  puts args.csv_file
+  puts args.bank
+  puts args.type
 end
