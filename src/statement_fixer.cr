7d8b2ba7a1baeb0b743a5503bedf9be346608f0b
Karthik T <karthikt.holmes+github@gmail.com>
2017-07-17 22:59:51 +0800

    Minor fixes to get DBS csv working
    Too bad it is a manual copy paste task to get the data..

--- src/statement_fixer.cr
+++ src/statement_fixer.cr
@@ -14,9 +14,6 @@ end
 
 module StatementFixer
   args = Options.parse(ARGV)
-  puts args.csv_file
-  puts args.bank
-  puts args.type
 
   unless File.exists? args.csv_file
     raise "Specified file does not exist - #{args.csv_file}"
@@ -28,5 +25,5 @@ module StatementFixer
   end
 
   parser = ParserRegister.types[parser_key].new(args.csv_file)
-  parser.parse_into("parser_key")
+  parser.parse_into(parser_key)
 end
--- src/statement_fixer/parsers/dbs_credit_card_parser.cr
+++ src/statement_fixer/parsers/dbs_credit_card_parser.cr
@@ -4,10 +4,10 @@ class DBSCreditCardParser < CSVParser
   register "dbs_credit_card", self
 
   def _parse_line(line)
-    date = Time.parse(line[0], "%d-%b-%y") rescue nil
+    date = Time.parse(line[0], "%d %b %Y") rescue nil # %d-%b-%y
     return unless date
     income, expense = if line[2].ends_with? "cr"
-                        [line[2][2..-1].to_f, 0]
+                        [line[2][2..-3].gsub(",", "").to_f, 0]
                       else
                         [0, line[2][2..-1].to_f]
                       end
